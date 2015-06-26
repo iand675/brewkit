b504baf33c34fb4bb9b99c6e0dd700362a34d4be
Ian Duncan <ian@iankduncan.com>
2015-06-26 00:34:39 -0400

    feature(Main): parse changelog from Git
    Start adding changelog support.
    
    N/A

--- src/Main.hs
+++ src/Main.hs
@@ -1,21 +1,32 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 import           Control.Monad
-import           Data.Attoparsec.Text
+import           Control.Monad.Trans
+import           Text.PrettyPrint.ANSI.Leijen (Doc, putDoc, indent)
+import           Text.Trifecta
+import           Text.Trifecta.Delta
 import qualified Data.List    as L
 import           Data.Monoid
 import           Data.Text (Text)
 import qualified Data.Text    as T
+import           Data.Text.Encoding
 import qualified Data.Text.IO as T
-import           Options.Applicative hiding (Success, Failure)
+import qualified Data.ByteString.Char8 as B
+import           Data.ByteString.UTF8 as UTF8
+import           Options.Applicative hiding (Success, Failure, Parser)
+import           System.Console.ANSI
 import           System.Directory
 import           System.Exit
+import           System.FilePath
+import           System.IO
+import           System.Process
 import           Prelude             hiding (takeWhile)
 
 data Command
   = InstallHooks
   | ValidateCommitMessage FilePath
-  deriving (Read, Show)
+  | Changelog
+  deriving (Show)
 
 validateCommitArgs = ValidateCommitMessage
   <$> (argument str (metavar "COMMIT_MSG_PATH"))
@@ -23,6 +34,7 @@ validateCommitArgs = ValidateCommitMessage
 commandParser = subparser
   (  command "install" (info (pure InstallHooks) (progDesc "Install Git hooks for the brewtown workflow" ))
   <> command "validate:commit:message" (info validateCommitArgs (progDesc "Validate a commit message"))
+  <> command "changelog" (info (pure Changelog) (progDesc "Generate changelog"))
   )
 
 data CommitType = Feature
@@ -33,7 +45,7 @@ data CommitType = Feature
                 | Perf
                 | Test
                 | Chore
-                deriving (Show, Eq)
+                deriving (Show, Eq, Ord)
 
 data CommitMessage = CommitMessage
   { commitMessageType    :: CommitType
@@ -43,80 +55,131 @@ data CommitMessage = CommitMessage
   , commitMessageFooter  :: [Text]
   } deriving (Show)
 
-data ValidationFailure = ParseFailure String
-                       | HeaderTooLong
-                       | LineTooLong Text
-                       deriving (Show)
-
 lineLength = 100
 
-validateCommitMessage raw = case parseCommitMessage raw of
-  Left err -> Left [ParseFailure err]
-  Right msg -> let errs = (validateHeader msg <> validateBody msg <> validateFooter msg) in case errs of
-    [] -> Right msg
-    es -> Left es
+validLength section = do
+  bl <- line
+  let tl = T.length $ decodeUtf8 bl
+  if tl > lineLength
+    then raiseErr $ failed (section ++ " line too long. Maximum is " ++ show lineLength ++ " chars, got " ++ show tl ++ " chars.")
+    else return ()
 
-validateHeader m = if (4 + length (show $ commitMessageType m) + T.length (commitMessageScope m) + T.length (commitMessageSubject m)) <= lineLength
-  then []
-  else [HeaderTooLong]
+match k v = string k *> pure v
 
-validateFooter m = concatMap (\l -> if T.length l <= lineLength then [] else [LineTooLong l]) (commitMessageFooter m)
+typeParser = choice $ map (uncurry match)
+  [ ("feature", Feature)
+  , ("fix", Fix)
+  , ("docs", Docs)
+  , ("style", Style)
+  , ("refactor", Refactor)
+  , ("perf", Perf)
+  , ("test", Test)
+  , ("chore", Chore)
+  ]
 
-validateBody   m = concatMap (\l -> if T.length l <= lineLength then [] else [LineTooLong l]) (commitMessageBody m)
+wrappedBy :: Char -> Char -> Parser String
+wrappedBy l r = between (char l) (char r) $ many $ notChar r
 
-typeParser = choice [ string "feature"  *> pure Feature
-                    , string "fix"      *> pure Fix
-                    , string "docs"     *> pure Docs
-                    , string "style"    *> pure Style
-                    , string "refactor" *> pure Refactor
-                    , string "perf"     *> pure Perf
-                    , string "test"     *> pure Test
-                    , string "chore"    *> pure Chore
-                    ]
+scopeParser :: Parser Text
+scopeParser = T.pack <$> wrappedBy '(' ')'
 
-scopeParser = do
-  char '('
-  s <- takeWhile (/= ')')
-  char ')'
-  return s
+endOfLine = optional (char '\r') >> char '\n' >> return ()
 
+notEOL :: Parser Char
+notEOL = noneOf "\r\n"
+
+nextSection :: Parser ()
+nextSection = endOfLine *> endOfLine *> pure ()
+
+commitMessageParser :: Parser CommitMessage
 commitMessageParser = do
   type_ <- typeParser <?> "Type"
   scope <- scopeParser <?> "Scope"
   string ": "
-  subject <- takeWhile1 (not . isEndOfLine) <?> "Subject"
+  subject <- T.pack <$> some notEOL <?> "Subject"
+  validLength "Subject"
   endOfLine
   endOfLine
-  body <- manyTill (takeWhile1 (not . isEndOfLine) <* endOfLine) endOfLine <?> "Body"
-  footer <- manyTill (takeWhile1 (not . isEndOfLine) <* endOfLine) (endOfLine <|> endOfInput) <?> "Footer"
+  body <- many $ do
+    res <- T.pack <$> some notEOL <?> "Body"
+    validLength "Body"
+    endOfLine
+    return res
+  endOfLine
+  footer <- many $ do
+    res <- T.pack <$> some notEOL <?> "Footer"
+    validLength "Footer"
+    endOfLine
+    return res
+  many endOfLine
   return $ CommitMessage type_ scope subject body footer
 
+main :: IO ()
 main = do
   args <- execParser $ info commandParser idm
   case args of
-    InstallHooks -> do
-      installCommitMessageHook
-    ValidateCommitMessage path -> do
-      T.putStrLn "Validating commit message format..."
-      msg <- T.readFile path
-      case validateCommitMessage msg of
-        Left errs -> do
-          forM_ errs $ \err -> T.putStrLn $ case err of
-            ParseFailure str -> "Message parse failure: " <> T.pack str
-            HeaderTooLong    -> "Header is too long"
-            LineTooLong l    -> "Line too long: " <> l
-          T.putStrLn "Failing message:"
-          T.putStrLn msg
-          exitFailure
-        Right _ -> return ()
+    InstallHooks -> installCommitMessageHook
+    ValidateCommitMessage path -> B.readFile path >>= validateCommitMessage path
+    Changelog -> return ()
 
-parseCommitMessage = parseOnly commitMessageParser
+parseCommitMessage :: Text -> IO ()
+parseCommitMessage = parseTest commitMessageParser . T.unpack
+
+validateCommitMessage :: FilePath -> B.ByteString -> IO ()
+validateCommitMessage path str = do
+  T.putStr "Checking commit message format "
+  let draftPath = replaceBaseName path (takeBaseName path ++ "_DRAFT")
+  case parseByteString (commitMessageParser <* eof) (Directed (UTF8.fromString path) 0 0 0 0) str of
+    Failure doc -> do
+      setSGR [SetColor Foreground Dull Red]
+      T.putStrLn "✗"
+      setSGR [Reset]
+      putDoc $ indent 2 doc
+      T.putStr "\n"
+      copyFile path draftPath
+      exitFailure
+    _ -> do
+      setSGR [SetColor Foreground Dull Green]
+      T.putStrLn "✓"
+      setSGR [Reset]
 
 test :: Text
 test = "fix(Network.Mandrill): use correct lens names\n\nIt was broken, now it isn't.\nNot too tough to fix, really.\n\nFixes #1234\n"
 
+installCommitMessageHook :: IO ()
 installCommitMessageHook = do
   let commitMsgHookPath = ".git/hooks/commit-msg"
   T.writeFile commitMsgHookPath "#!/bin/sh\nbrewkit validate:commit:message $1\n"
   perms <- getPermissions commitMsgHookPath
   setPermissions commitMsgHookPath $ perms { executable = True }
+
+gitLogOutputs :: Parser [CommitMessage]
+gitLogOutputs = sepBy1 commitMessageParser $ string "----END_OF_COMMIT----"
+
+gitLogMessages :: IO Handle
+gitLogMessages = do
+  (_, Just out, _, _) <- createProcess $ p { std_out = CreatePipe }
+  return out
+  where
+    p = proc "git" ["log", "--format=\"%B%n----END_OF_COMMIT----\""]
+
+parseFromHandle :: MonadIO m => Parser a -> Delta -> Handle -> m (Result a)
+parseFromHandle p d h = go $ stepParser startP mempty mempty
+  where
+    startP = release d *> p
+    go aStep = do
+      bs <- liftIO $ B.hGet h 4096
+      if bs == B.empty
+        then return $ starve aStep
+        else do
+          let aStep' = feed bs aStep
+          case aStep' of
+            StepFail _ _ -> return $ starve aStep'
+            _ -> go aStep'
+  
+{-
+installMessageTemplateHook = do
+  let templateHookPath = ".git/hooks/prepare-commit-msg"
+  T.writeFile templateHookPath "#!/bin/sh/\n"
+-}
+
